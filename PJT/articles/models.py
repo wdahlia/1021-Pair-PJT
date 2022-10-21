7b4f19be15e0ba9cecf26a9688ed9ff2358cd5df
dahlia <wlstnrdahlia@gmail.com>
2022-10-21 11:19:57 +0900

    create review part

--- PJT/articles/models.py
+++ PJT/articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
+
+class Review(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    movie_name = models.CharField(max_length=30)
+    grade = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- PJT/articles/views.py
+++ PJT/articles/views.py
@@ -1,7 +1,66 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Review
+from .forms import ReviewForm
 # Create your views here.
 
 
 def index(request):
-    return render(request, "articles/index.html")
+    reviews = Review.objects.order_by('-id')
+    
+    context = {
+       'reviews' : reviews,
+    }
+    
+    return render(request, "articles/index.html", context)
+
+
+def create(request):
+    if request.method == "POST":
+        create_form = ReviewForm(request.POST)
+        if create_form.is_valid():
+            create_form.save()
+            return redirect('articles:index')
+
+    else:
+        create_form = ReviewForm()
+    
+    context = {
+        'create_form' : create_form,
+    }
+
+    return render(request, "articles/create.html", context)
+            
+
+def delete(request, review_pk):
+    Review.objects.get(pk=review_pk).delete()
+
+    return redirect("articles:index")
+
+
+def detail(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+
+    context = {
+        'review' : review,
+    }
+
+    return render(request, "articles/detail.html", context)
+
+
+def update(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    
+    if request.method == "POST":
+        update_form = ReviewForm(request.POST, instance=review)
+        if update_form.is_valid():
+            update_form.save()
+            return redirect("articles:index")
+
+    else:
+        update_form = ReviewForm(instance=review)
+    
+    context = {
+        'update_form' : update_form,
+    }
+
+    return render(request, "articles/update.html", context)
